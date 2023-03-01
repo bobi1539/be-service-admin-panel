2dcaeabd31f2deacf229645f44c6e4c0544d6d6f
bobi.rival <bobi.rival@dwidasa.com>
2023-03-01 16:24:49 +0700

    init project

---
+++ src/main/java/com/zero/admin/panel/BeServiceAdminPanelApplication.java
@@ -0,0 +1,13 @@
+package com.zero.admin.panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeServiceAdminPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeServiceAdminPanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero/admin/panel/BeServiceAdminPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.admin.panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeServiceAdminPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
